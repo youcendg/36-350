0264f18d1acffa4bda722ff9e0bcfa19fa62109f
Youce Ji <youcendg@gmail.com>
2017-12-07 11:16:14 -0500

    fixed message

--- alien-message.R
+++ alien-message.R
@@ -1,2 +1,4 @@
-cat("Yo Aliens\n what is up man, I hope you guys are doing well.
-    If you can see and understand this, u guys probably can easily attack us with\nmore advanced technologies and occupy earth one day maybe. But please have mercy, I don't want to die yet :)")
+cat("Yo Aliens\nwhat is up man, I hope you guys are doing well.
+    If you can see and understand this, u guys probably can easily attack us with 
+    more advanced technologies and occupy earth one day maybe. But please have mercy, 
+    I don't want to die yet :)")
